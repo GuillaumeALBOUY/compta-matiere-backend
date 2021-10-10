edab820ad06a48d1281e4a6911f8aeb4963459e7
GuillaumeAlbouy <guillaume.albouy82@gmail.com>
2021-10-10 16:13:59 +0200

    ajout du type de matiere et de son archivage

--- models/matiere.js
+++ models/matiere.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const matiereSchema = mongoose.Schema({
+  type: { type: String},
   nom: { type: String, required: true },
   ref: { type: String, required: true, unique: true },
   fournisseur: { type: String, required: true },
   lot: { type: String, required: true },
   dlc: { type: Date, required: true},
   qCommande: { type: Number, required: true },
+  archive: { type: Boolean},
   mouvements : [{ 
     quantite : {type : Number, required: true},
     date : {type : Date, required: true},
